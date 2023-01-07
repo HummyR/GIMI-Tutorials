6e59d22a187975c920a2be33087840268e9b5b82
HummyR <119277434+HummyR@users.noreply.github.com>
2023-01-07 02:07:42 -0500

    Update AGMGVer3ShaderFixer.py

--- AGMGVer3ShaderFixer.py
+++ AGMGVer3ShaderFixer.py
@@ -2,28 +2,107 @@ import os
 import re
 import sys
 
-path = getattr(sys, '_MEIPASS', os.getcwd())
-full_path = path+"\\data"
-data_path = full_path+"\\DISABLED_Shaders.ini"
+# Script created by HummyR#8131, Modder4869#4818, and Takoyaki#0697.
+# Huge thanks to SilentNightSound#7430 for guiding us.
+# Please visit our AGMG discord server https://discord.gg/agmg if you have any questions regarding this.
 
 def main():
     
     print("\nAnime Game 3.0+ Shader Fixer\n")
     
-    print("Be sure to use mods generated from GIMI importer plugin.\n\n")
+    print("Be sure to use mods generated from GIMI importer plugin.\nPlease visit our AGMG discord server https://discord.gg/agmg if you have any questions regarding this.")
+    print("Source code: https://github.com/HummyR/GIMI-Tutorials/blob/main/Version_3_Character_Shader_Fix.md")
     
+    print("\nThese files will be modified:")
+    for roote, dir, testfiles in os.walk("."):
+        if "disabled" in roote.lower():
+            continue
+        for fil in testfiles:
+            if "disabled" in fil.lower():
+                continue
+            if os.path.splitext(fil)[1] == ".ini":
+                print(os.path.join(roote, fil))
     try:
-        input("Press enter to continue\n")
+        input("\nThis will modify all .ini files in this folder and subdirectories. \nRename some with 'disabled' if you dont want that .ini edited. \nPress enter to continue\n")
     except:
-        raw_input("Press enter to continue")
+        raw_input("\nThis will modify all .ini files in this folder and subdirectories. \nRename some with 'disabled' if you dont want that .ini edited. \nPress enter to continue\n")
     
-    
-    with open(data_path, "r") as f:
-        print("Initializing...\n")  
-        Shadercode = f.read()
+  
+    Shadercode = """
+; Generated shader fix for 3.0+ GIMI importer characters. Please contact the tool developers at https://discord.gg/agmg if you have any questions.
+
+; Variables -----------------------
+
+[Constants]
+global $CharacterIB
+;0=none, 1=head, 2=body, 3=dress, 4=extra, etc.
+
+[Present]
+post $CharacterIB = 0
+
+[ResourceRefHeadDiffuse]
+[ResourceRefHeadLightMap]
+[ResourceRefBodyDiffuse]
+[ResourceRefBodyLightMap]
+[ResourceRefDressDiffuse]
+[ResourceRefDressLightMap]
+[ResourceRefExtraDiffuse]
+[ResourceRefExtraLightMap]
+
+; ShaderOverride ---------------------------
+
+[ShaderRegexCharReflection]
+shader_model = ps_5_0
+run = CommandListReflectionTexture
+[ShaderRegexCharReflection.pattern]
+discard_n\w+ r\d\.\w+\\n
+lt r\d\.\w+, l\(0\.010000\), r\d\.\w+\\n
+and r\d\.\w+, r\d\.\w+, r\d\.\w+\\n
+
+[ShaderRegexCharOutline]
+shader_model = ps_5_0
+run = CommandListOutline
+[ShaderRegexCharOutline.pattern]
+mov o0\.w, l\(0\)\\n
+mov o1\.xyz, r0\.xyzx\\n
+
+; CommandList -------------------------
+
+[CommandListReflectionTexture]
+if $CharacterIB != 0
+    if $CharacterIB == 1
+        ps-t0 = copy ResourceRefHeadDiffuse
+    else if $CharacterIB == 2
+        ps-t0 = copy ResourceRefBodyDiffuse
+    else if $CharacterIB == 3
+        ps-t0 = copy ResourceRefDressDiffuse
+    else if $CharacterIB == 4
+        ps-t0 = copy ResourceRefExtraDiffuse    
+    endif
+drawindexed=auto
+$CharacterIB = 0
+endif
+
+[CommandListOutline]
+if $CharacterIB != 0
+    if $CharacterIB == 1
+        ps-t1 = copy ResourceRefHeadLightMap
+    else if $CharacterIB == 2
+        ps-t1 = copy ResourceRefBodyLightMap
+    else if $CharacterIB == 3
+        ps-t1 = copy ResourceRefDressLightMap
+    else if $CharacterIB == 4
+        ps-t1 = copy ResourceRefExtraLightMap
+    endif
+drawindexed=auto
+$CharacterIB = 0
+endif
+"""
              
 
     ModifiedFiles = []
+    DISABLED_files = []
+    Corecode = []
     for root, dir, files in os.walk("."):
         if "disabled" in root.lower():
             continue
@@ -31,51 +110,68 @@ def main():
             if "disabled" in file.lower():
                 continue
             if os.path.splitext(file)[1] == ".ini":
-                ModifiedFiles.append(file)
-            
-                print(file)
-
-                with open(file, "r") as f:
-                    Corecode = f.read()
-                    Newcode = Corecode
+                file_path = os.path.join(root, file)
+                DISABLED_file_path = os.path.join(root, "DISABLED_" + file)
+                
+                with open(file_path, "r") as f:
+                    Original = f.read()
+                    Corecode.append(Original)
+                    Newcode = Original
                     ObjectPart = ['Head', 'Body', 'Dress', 'Extra']
                     
                     for i in range(len(ObjectPart)):
                         OPIterate = ObjectPart[i]
-                        TOIndex = re.search('\[TextureOverride\w+' + OPIterate + '\]\n', Newcode)
-                        
-                        if TOIndex == None:
+                        TO = re.search('\[TextureOverride\w+' + OPIterate + '\]\n', Newcode)
+                        if TO == None:
                             continue
-                            
-                        x = TOIndex.end()
-                        Newcode = Newcode[:x] + '$CharacterIB = ' + str(i+1) + '\nResourceRef' + OPIterate + 'Diffuse = reference ps-t1' + '\nResourceRef' + OPIterate + 'LightMap = reference ps-t2\n'  + Newcode[x:]
 
-                    result = Newcode 
+                        TOIndex = TO.end()
+
+                        NextCommand = Newcode.find('[', TOIndex)
+ 
+
+                        x = re.search('(?s:.*)^ps-t(?:.(?!^ps-t))+$', Newcode[TOIndex:NextCommand], re.MULTILINE)
+                        y = re.search('(?s:.*)^run(?:.(?!^run))+$', Newcode[TOIndex:NextCommand], re.MULTILINE)
+
+                        if x == None and y == None:
+                            continue
+                        if x == None: End = y.end() + TOIndex
+                        if y == None: End = x.end() + TOIndex
+                        End = max(x, y)
+
+                        Newcode = Newcode[:End] + '\n$CharacterIB = ' + str(i+1) + '\nResourceRef' + OPIterate + 'Diffuse = reference ps-t1' + '\nResourceRef' + OPIterate + 'LightMap = reference ps-t2'  + Newcode[End:]
+                    ModifiedFiles.append(file_path)
+                    DISABLED_files.append(DISABLED_file_path)
+                        
+                    result = Newcode
                     result += "\n\n; Version 1.0.0 AGMG Tool Developer Version 3 Shader Fixer\n\n"
                     result += Shadercode
                         
-                    with open("DISABLED_"+file, "w") as f:
-                        f.write(Corecode)                       
+                    with open(DISABLED_file_path, "w") as f:
+                        f.write(Original)                       
                     
-                    with open(file, "w") as f:
+                    with open(file_path, "w") as f:
                         f.write(result)
                         print("Writing shader fix to " + file + " Completed.\n")
                     
-    undo = input("Undo operation? [y] \nDo not modify file names while you are checking. \nExit if you are done.\n")
+    try:
+        undo = input("Undo operation? [y] \nDo not modify file names while you are checking. \nExit if you are done.\n")
+    except:
+        raw_input("Undo operation? [y] \nDo not modify file names while you are checking. \nExit if you are done.\n")
         
     if undo == "y":
         print("\n")
-        for modded in ModifiedFiles:
-            if os.path.exists(modded):
-                with open(modded, "w") as f:
-                    f.write(Corecode)
-                    os.remove("DISABLED_" + modded)
-                    print("Undo complete")
+        for modded in range(len(ModifiedFiles)):
+        
+            mod = ModifiedFiles[modded]
+            
+            if os.path.exists(mod):
+                with open(mod, "w") as f:
+                    f.write(Corecode[modded])
+                    os.remove(DISABLED_files[modded])
+                    print(mod + " undo complete.")
             else:
                 print("The file does not exist")
-
-            
-             
 
 
 if __name__ == "__main__":
